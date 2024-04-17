ac0376dc47b5e231ef18c91fbf1154487aeba880
Pedro Villegas <Pedrovigut@hotmail.com>
2024-04-16 23:26:54 -0400

    began to add home page to glaze hardware's app

--- market.py
+++ market.py
@@ -11,17 +11,24 @@ app = Flask(__name__)
 
 @app.route("/about")
 def about_page():
-    return '<h1>This is the about page</h1>'
+    username = "Pedro"
+    return render_template("about.html",current_user=username)
 
 ## dynamic routes, taking a varaible and using it as a route
 ## need the <varibale_name> format int the route section
 @app.route("/about/<username>")
 def about_user_page(username):
-    ## needs to be a formatted print statement 
+    ## needs to be a formatted print statement
+    username = "Pedro" 
     return f'<h1>This is now the baout page of whatever is put in the url page, basically up to the user {username}</h1>'
 
 ##home page for the application
 ## need render_tempplates function from flask package, need a astring argument
 @app.route("/")
+@app.route("/home")
 def home_page():
     return render_template("home.html")
+
+@app.route("/Products")
+def products_page():
+    return render_template("products.html", product="Door handle")
